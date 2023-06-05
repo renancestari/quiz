d33dfbaecfca52f40182b948ca5733b03c37350f
renancestari <rnncestari@gmail.com>
2023-06-05 17:06:05 -0300

    Enviando projeto

---
+++ app/data.js
@@ -0,0 +1,35 @@
+export const quiz = {
+    totalQuestions: 5,
+    questions: [
+        {
+            id: 1,
+            question: 'Qual é o principal componente de um computador responsável pelo processamento de dados?',
+            answers: ['Memória', 'Placa de vídeo', 'Processador', 'Disco rígido'],
+            correctAnswer: 'Processador',
+        },
+        {
+            id: 2,
+            question: 'O que significa a sigla "RAM" em relação a um computador?',
+            answers: ['Registro', 'Acesso', 'Armazenamento', 'Memória'],
+            correctAnswer: 'Memória',
+        },
+        {
+            id: 3,
+            question: 'O que é um sistema operacional?',
+            answers: ['Programa', 'Software', 'Hardware', 'Aplicativo'],
+            correctAnswer: 'Software',
+        },
+        {
+            id: 4,
+            question: 'O que é um arquivo com a extensão ".jpg"?',
+            answers: ['Imagem', 'Texto', 'Áudio', 'Vídeo'],
+            correctAnswer: 'Imagem',
+        },
+        {
+            id: 5,
+            question: 'O que é um disco rígido?',
+            answers: ['Processador', 'Armazenamento', 'Memória', 'Placa-mãe'],
+            correctAnswer: 'Armazenamento',
+        },
+    ],
+};
